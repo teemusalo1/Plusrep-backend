9d71b3292ab74b2f72f81a5de91df0bfa2bea720
Miska Nghipandulwa <miskanghipandulwa@ymail.com>
2022-03-22 11:06:13 +0200

    front-end

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
