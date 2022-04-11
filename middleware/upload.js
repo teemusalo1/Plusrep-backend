8a42dbcbe6433131bd2ab0a8411b842b75b9d052
teemusalo1 <teemu.matias.salo@gmail.com>
2022-04-11 12:05:13 +0300

    now image stream from db is working :)

---
+++ middleware/upload.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+const multer = require('multer')
+const { GridFsStorage } = require('multer-gridfs-storage')
+
+const storage = new GridFsStorage({
+  url: process.env.MONGODB_URI2,
+  options: { useNewUrlParser: true, useUnifiedTopology: true },
+  file: (req, file) => {
+    const match = ['image/png', 'image/jpeg']
+
+    if (match.indexOf(file.mimetype) === -1) {
+      const filename = `${Date.now()}-any-name-${file.originalname}`
+      return filename
+    }
+
+    return {
+      bucketName: 'photos',
+      filename: `${Date.now()}-any-name-${file.originalname}`,
+    }
+  },
+})
+
+module.exports = multer({ storage })
--- routes/posts.js
+++ routes/posts.js
@@ -8,31 +8,8 @@ const express = require('express')
 const Post = require('../models/post')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const upload = require('../middleware/upload')
 const router = express.Router()
-const multer = require('multer')
-const { GridFsStorage } = require('multer-gridfs-storage')
-var fs = require('fs')
-var path = require('path')
-const storage = new GridFsStorage({
-  url: process.env.MONGODB_URI2,
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
-  file: (req, file) => {
-    const match = ['image/png', 'image/jpeg']
-
-    if (match.indexOf(file.mimetype) === -1) {
-      const filename = `${Date.now()}-any-name-${file.originalname}`
-      return filename
-    }
-
-    return {
-      filename: `${Date.now()}-any-name-${file.originalname}`,
-    }
-  },
-})
-
-var upload = multer({ storage: storage })
-
-
 const getTokenFrom = (request) => {
   const authorization = request.get('authorization')
   console.log('gettokenfrom req', authorization)
@@ -53,9 +30,11 @@ router.get('/api/posts', async (request, response, next) => {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
 
+
     await Post.find({})
       .populate('author')
       .populate('comments')
+      .populate('image')
       .then((posts) => {
         response.json(posts)
       })
@@ -63,25 +42,32 @@ router.get('/api/posts', async (request, response, next) => {
   }
 })
 
-router.post('/api/posts', async (request, response) => {
+router.post('/api/posts',upload.single('file'), async (request, response) => {
   const body = request.body
   const user = await User.findById(body.author)
   if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
-
+  let imgUrl = ''
+  try {
+    imgUrl = `http://localhost:3001/file/${request.file.filename}`
+  } catch (error) {
+    console.log(error)
+  }
   const post = new Post({
     author: user._id,
     content: body.content,
     date: new Date(),
-    image: { data: upload.single('image'),
-      contentType: 'image/png' }
+    image: imgUrl
   })
 
   const savedPost = await post.save()
   user.post = user.post.concat(savedPost)
   await user.save()
   response.json(savedPost.toJSON)
+
 })
+
+
 
 module.exports = router
