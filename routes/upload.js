456d27e9f02477acea94669cf484d6f3569c6d41
Miska Nghipandulwa <miskanghipandulwa@ymail.com>
2022-04-20 10:35:41 +0300

    new build

--- routes/upload.js
+++ routes/upload.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 router.post('/upload', upload.single('file'), async (req, res) => {
   if (req.file === undefined) return res.send('you must select a file.')
-  const imgUrl = `https://thawing-fjord-30792.herokuapp.com/${req.file.filename}`
+  const imgUrl = `http://localhost:3001/file/${req.file.filename}`
   return res.send(imgUrl)
 })
 
